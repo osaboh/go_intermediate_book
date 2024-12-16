033829526d9722d41b6b78d7ce03283403fa4b57
Mikio Osanai <osaboh@gmail.com>
2024-12-17 03:07:15 +0900

    3-2

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"go_http_book/handlers"
-
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser,
+		dbPassword, dbDatabase)
+
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("connect to DB")
+	}
+	panic("ok")
 
 	r := mux.NewRouter()
 
