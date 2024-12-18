04e60423c063beffbddaa470b0ab31d89271f671
Mikio Osanai <osaboh@gmail.com>
2024-12-19 01:58:59 +0900

    3-7

--- main.go
+++ main.go
@@ -11,7 +11,45 @@ import (
 	"net/http"
 )
 
-func main() {
+func insert() {
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser,
+		dbPassword, dbDatabase)
+
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	// データを挿入する処理
+	article := models.Article{
+		Title:    "insert test",
+		Contents: "Can I insert data correctly?",
+		UserName: "osaboh",
+	}
+
+	const sqlStr = `
+		insert into articles (title, contents, username, nice, created_at) values
+		(?, ?, ?, 0, now())
+	`
+
+	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+	if err != nil {
+		fmt.Println(err)
+		return
+
+	}
+
+	fmt.Println(result.LastInsertId())
+	fmt.Println(result.RowsAffected())
+
+	return
+}
+
+func scan() {
 
 	dbUser := "docker"
 	dbPassword := "docker"
@@ -53,6 +91,72 @@ func main() {
 	}
 
 	fmt.Printf("%+v\n", article)
+
+}
+
+func transaction() {
+
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser,
+		dbPassword, dbDatabase)
+
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	// トランザクション開始
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 現在のいいねを取得する
+	article_id := 1
+	const sqlGetNice = `
+		SELECT nice FROM articles WHERE article_id = ?;
+	`
+	row := tx.QueryRow(sqlGetNice, article_id)
+	if err := row.Err(); err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	// 変数 nicenum に現在のいいねを読み込む
+	var nicenum int
+	err = row.Scan(&nicenum)
+	if err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	// いいねをインクリメント
+
+	const sqlUpdateNice = `
+		UPDATE articles SET nice = ? WHERE article_id = ?
+	`
+	row = tx.QueryRow(sqlUpdateNice, nicenum+1, article_id)
+	if err := row.Err(); err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	// コミットして確定
+	tx.Commit()
+}
+
+func main() {
+
+	insert()
+	scan()
+	transaction()
 
 	r := mux.NewRouter()
 
