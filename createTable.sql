7d2ced01f4a4fc9cab6d8675f0289af350a28c2c
Mikio Osanai <osaboh@gmail.com>
2024-12-18 21:48:24 +0900

    task sqlfluff

--- createTable.sql
+++ createTable.sql
@@ -1,18 +1,18 @@
 # 記事データを格納するためのテーブル
 CREATE TABLE IF NOT EXISTS articles (
-	   article_id integer unsigned auto_increment primary key,
-	   title varchar(100) not null,
-	   contents text not null,
-	   username varchar(100) not null,
-	   nice integer not null,
-	   created_at datetime
+    article_id integer unsigned AUTO_INCREMENT PRIMARY KEY,
+    title varchar(100) NOT NULL,
+    contents text NOT NULL,
+    username varchar(100) NOT NULL,
+    nice integer NOT NULL,
+    created_at DATETIME
 );
 
 # コメントデータを格納するためのテーブル
 CREATE TABLE IF NOT EXISTS comments (
-	   comment_id integer unsigned auto_increment primary key,
-	   article_id integer unsigned not null,
-	   message text not null,
-	   created_at datetime,
-	   foreign key (article_id) references articles(article_id)
+    comment_id integer unsigned AUTO_INCREMENT PRIMARY KEY,
+    article_id integer unsigned NOT NULL,
+    message text NOT NULL,
+    created_at DATETIME,
+    FOREIGN KEY (article_id) REFERENCES articles (article_id)
 );
